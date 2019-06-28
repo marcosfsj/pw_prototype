abc89caaf8cb928594a34093e3a285bc2a75018a
marcfernandes <marcfernandes@teksystems.com>
2019-06-28 11:36:41 -0400

    another commit

--- src/config/routing/routes.jsx
+++ src/config/routing/routes.jsx
@@ -1,6 +1,8 @@
 import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import { ProtectedRoute } from './protected-route';
+
 const Prototype = lazy(() => import('../../features/prototype/prototype'));
 const PrototypeTwo = lazy(() => import('../../features/prototype-two/prototype-two'));
 const Comments = lazy(() => import('../../features/comments/comments'));
@@ -9,9 +11,11 @@ const routes = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
-        <Route exact path="/Prototype" component={Prototype} />
-        <Route exact path="/PrototypeTwo" component={PrototypeTwo} />
-        <Route exact path="/Comments" component={Comments} />
+        <Route exact path="/prototype" component={Prototype} />
+        <Route exact path="/prototypeTwo" component={PrototypeTwo} />
+        <Route exact path="/comments" component={Comments} />
+        <ProtectedRoute exact path="/app" component={() => "protected"} />
+        <Route path="*" component={() => "404 NOT FOUND"} />
       </Switch>
     </Suspense>
   );
